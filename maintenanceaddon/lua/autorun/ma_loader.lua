7c9d0d62734d99848449ed53f8d24837612347e6
Zozo <Zozo832@users.noreply.github.com>
2017-02-19 22:22:48 +0100

    Update ma_loader.lua

--- maintenanceaddon/lua/autorun/ma_loader.lua
+++ maintenanceaddon/lua/autorun/ma_loader.lua
@@ -1,3 +1,24 @@
+/*
+
+	French :
+
+	Créateur : "Zozo832" sur "github.com" ( https://github.com/Zozo832 ) et "Zozo147" sur "mtxserv.fr" ( https://www.mtxserv.fr/forums/members/zozo147.45708/ )
+	
+	Ce travail est sous la licence Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0). 
+	
+	Pour voir une copie de cette licence veuillez vous rendre sur : https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+	
+	English :
+	
+	Creator : "Zozo832" on "github.com" ( https://github.com/Zozo832 ) and "Zozo147" on "mtxserv.fr" ( https://www.mtxserv.fr/forums/members/zozo147.45708/ )
+	
+	This work is licensed under the 
+	Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
+	
+	To view a copy of this license please go to : https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+
+*/
+
 if SERVER then
 
 	include("ma/server/sv_main.lua")
